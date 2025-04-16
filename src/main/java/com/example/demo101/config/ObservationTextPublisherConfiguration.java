2a28d767d58d86c648d8e2e539052961113584de
Tanapat <a_little_kids@hotmail.com>
2025-04-17 00:51:31 +0900

    add Actuator

---
+++ src/main/java/com/example/demo101/config/ObservationTextPublisherConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demo101.config;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import io.micrometer.observation.ObservationTextPublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class ObservationTextPublisherConfiguration {
+
+    @Bean
+    public ObservationHandler<Observation.Context> observationTextPublisher() {
+        return new ObservationTextPublisher(log::info);
+    }
+}
